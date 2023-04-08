861d43245185eff5e20542dca304d9098742a173
Danislav Konyarski <unstopable46@gmail.com>
2023-04-08 11:54:27 +0300

    new module

---
+++ StacksAndQueues/src/StacksAndQueuesLab/T1BrowserHistory.java
@@ -0,0 +1,10 @@
+package StacksAndQueuesLab;
+
+import java.util.Scanner;
+
+public class T1BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
