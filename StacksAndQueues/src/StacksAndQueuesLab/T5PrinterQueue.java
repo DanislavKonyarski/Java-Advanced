a12c8e80dcfb09b0d3e6f40311f78f2363c2ac60
Danislav Konyarski <unstopable46@gmail.com>
2023-04-10 16:07:17 +0300

    new task

---
+++ StacksAndQueues/src/StacksAndQueuesLab/T5PrinterQueue.java
@@ -0,0 +1,29 @@
+package StacksAndQueuesLab;
+
+import javax.security.sasl.SaslClient;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class T5PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        Deque<String> printFilesDeq = new ArrayDeque<>();
+        while (!input.equals("print")){
+            if (!input.equals("cancel")){
+                printFilesDeq.add(input);
+            }else {
+                if (printFilesDeq.size()==0){
+                    System.out.println("Printer is on standby");
+                }else {
+                    System.out.println("Canceled "+printFilesDeq.poll());
+                }
+            }
+            input = scanner.nextLine();
+        }
+        for (String s : printFilesDeq) {
+            System.out.println(printFilesDeq.poll());
+        }
+    }
+}
