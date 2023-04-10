0a0c14a709c2e24f847730dc7fbb174ded6f5e3f
Danislav Konyarski <unstopable46@gmail.com>
2023-04-10 14:09:04 +0300

    new task

--- StacksAndQueues/src/StacksAndQueuesLab/T1BrowserHistory.java
+++ StacksAndQueues/src/StacksAndQueuesLab/T1BrowserHistory.java
@@ -1,10 +1,27 @@
 package StacksAndQueuesLab;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class T1BrowserHistory {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String input = scanner.nextLine();
+        Deque<String> urlDeq = new ArrayDeque<>();
+        while (!input.equals("Home")){
+            if (!input.equals("back")){
+                urlDeq.push(input);
+                System.out.println(urlDeq.peek());
+            }else {
+                if (urlDeq.size()>1){
+                    urlDeq.pop();
+                System.out.println(urlDeq.peek());
+                }else {
+                    System.out.println("no previous URLs");
+                }
+            }
+            input = scanner.nextLine();
+        }
     }
 }
