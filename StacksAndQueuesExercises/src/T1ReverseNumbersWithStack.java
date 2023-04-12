37008b0674d992333cc0a2eb32c7f238d37d8865
Danislav Konyarski <unstopable46@gmail.com>
2023-04-12 10:09:18 +0300

    new task

--- StacksAndQueuesExercises/src/T1ReverseNumbersWithStack.java
+++ StacksAndQueuesExercises/src/T1ReverseNumbersWithStack.java
@@ -1,2 +1,18 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Scanner;
+
 public class T1ReverseNumbersWithStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        Deque<Integer> reversNumber = new ArrayDeque<>();
+        for (int number : numbers) {
+            reversNumber.push(number);
+        }
+        for (Integer integer : reversNumber) {
+            System.out.print(integer+" ");
+        }
+    }
 }
