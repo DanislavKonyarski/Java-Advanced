22eb3410c081c9d191a9c4b9a080918103422a2b
Danislav Konyarski <unstopable46@gmail.com>
2023-05-02 12:07:57 +0300

    update

---
+++ SetsAndMapsAdvanced/src/SetsAndMapsAdvancedExercises/T1UniqueUsernames.java
@@ -0,0 +1,10 @@
+package SetsAndMapsAdvancedExercises;
+
+import java.util.Scanner;
+
+public class T1UniqueUsernames {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
