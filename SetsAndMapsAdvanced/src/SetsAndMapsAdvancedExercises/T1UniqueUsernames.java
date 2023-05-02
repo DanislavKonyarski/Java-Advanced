d7866499d18a3d64b45b554ba59329eccb5f47ed
Danislav Konyarski <unstopable46@gmail.com>
2023-05-02 13:52:00 +0300

    new task

--- SetsAndMapsAdvanced/src/SetsAndMapsAdvancedExercises/T1UniqueUsernames.java
+++ SetsAndMapsAdvanced/src/SetsAndMapsAdvancedExercises/T1UniqueUsernames.java
@@ -1,10 +1,19 @@
 package SetsAndMapsAdvancedExercises;
 
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class T1UniqueUsernames {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int countInput = Integer.parseInt(scanner.nextLine());
+        Set<String> usernames = new LinkedHashSet<>();
+        for (int i = 0; i < countInput; i++) {
+            usernames.add(scanner.nextLine());
+        }
+        for (String username : usernames) {
+            System.out.println(username);
+        }
     }
 }
