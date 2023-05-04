9f539849e9876e5656eab787c1e6fcb0cf6142be
Danislav Konyarski <unstopable46@gmail.com>
2023-05-04 14:08:55 +0300

    new task

--- StreamsFilesAndDirectories/src/StreamsFilesAndDirectoriesLab/T2WriteToFile.java
+++ StreamsFilesAndDirectories/src/StreamsFilesAndDirectoriesLab/T2WriteToFile.java
@@ -1,4 +1,24 @@
 package StreamsFilesAndDirectoriesLab;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Scanner;
+
 public class T2WriteToFile {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String path = "StreamsFilesAndDirectories/resources/input.txt";
+        FileInputStream fileInputStream = new FileInputStream(path);
+        int byteStream = fileInputStream.read();
+        FileOutputStream fileOutputStream = new FileOutputStream("StreamsFilesAndDirectories/resources/02.WriteToFileOutput.txt");
+        while (byteStream >= 0) {
+            if (byteStream != '.' && byteStream != ',' && byteStream != '!' && byteStream != '?') {
+                fileOutputStream.write(byteStream);
+            }
+            byteStream = fileInputStream.read();
+        }
+    }
 }
