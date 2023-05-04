40d34676010e905b56dd5d85629d34eaa3ee9020
Danislav Konyarski <unstopable46@gmail.com>
2023-05-04 16:07:17 +0300

    new task

---
+++ StreamsFilesAndDirectories/src/StreamsFilesAndDirectoriesLab/T4ExtractIntegers.java
@@ -0,0 +1,25 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class T4ExtractIntegers {
+    public static void main(String[] args) throws FileNotFoundException {
+        String path = "StreamsFilesAndDirectories/resources/input.txt";
+        Scanner scanner = new Scanner(new FileInputStream(path));
+        PrintWriter printWriter = new PrintWriter("StreamsFilesAndDirectories/resources/04.ExtractIntegersOutput.txt");
+        while (scanner.hasNext()){
+            if (scanner.hasNextInt()){
+                int a = scanner.nextInt();
+                printWriter.println(a);
+            }
+            scanner.next();
+        }
+        scanner.close();
+        printWriter.close();
+
+    }
+}
