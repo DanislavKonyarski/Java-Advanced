92e0ca18778e9a5a9500041149c1ecd47b0c5686
Danislav Konyarski <unstopable46@gmail.com>
2023-05-04 11:40:05 +0300

    new module

---
+++ StreamsFilesAndDirectories/src/StreamsFilesAndDirectoriesLab/T1ReadFile.java
@@ -0,0 +1,9 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.util.Scanner;
+
+public class T1ReadFile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
