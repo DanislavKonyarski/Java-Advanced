e597dbc2eeb88336090d95bad798c9e143cdd0b7
Danislav Konyarski <unstopable46@gmail.com>
2023-05-05 10:01:41 +0300

    new task

--- StreamsFilesAndDirectories/src/StreamsFilesAndDirectoriesExercises/T1SumLines.java
+++ StreamsFilesAndDirectories/src/StreamsFilesAndDirectoriesExercises/T1SumLines.java
@@ -1,7 +1,20 @@
 package StreamsFilesAndDirectoriesExercises;
 
-public class T1SumLines {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class T1SumLines {
+    public static void main(String[] args) throws IOException {
+        String path = "StreamsFilesAndDirectories/Resources Exercises/input.txt";
+        FileInputStream fileInputStream = new FileInputStream(path);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(fileInputStream));
+        String line = reader.readLine();
+        while (line!=null){
+            int sum = 0;
+            for (int i = 0; i < line.length(); i++) {
+                sum+= line.charAt(i);
+            }
+            System.out.println(sum);
+            line = reader.readLine();
+        }
     }
 }
