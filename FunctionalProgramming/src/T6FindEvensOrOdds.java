2585006fba4508f1fba2b047890b4a6778a104e6
Danislav Konyarski <unstopable46@gmail.com>
2023-05-12 12:02:48 +0300

    new task

---
+++ FunctionalProgramming/src/T6FindEvensOrOdds.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class T6FindEvensOrOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = Integer.parseInt(scanner.next().split("\\s+")[0]);
+        int end = Integer.parseInt(scanner.nextLine().split("\\s+")[1]);
+        int[] input = IntStream.rangeClosed(start, end).toArray();
+        String oddEven = scanner.nextLine();
+
+        if (oddEven.equals("odd")) {
+            Arrays.stream(input).filter(e -> e % 2 != 0).forEach(e -> System.out.print(e + " "));
+        } else if (oddEven.equals("even")) {
+            Arrays.stream(input).filter(e -> e % 2 == 0).forEach(e -> System.out.print(e + " "));
+        }
+    }
+}
