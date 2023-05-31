bc89727993b95fcdca4701fe161bdd24d6c9fc41
Danislav Konyarski <unstopable46@gmail.com>
2023-05-31 12:54:40 +0300

    new task

--- Generics/src/GenericsExercises/CustomList.java
+++ Generics/src/GenericsExercises/CustomList.java
@@ -3,11 +3,57 @@ package GenericsExercises;
 import java.util.ArrayList;
 import java.util.List;
 
-public class  CustomList <T extends Comparable> {
+public class CustomList<T extends Comparable> {
     private List<T> customList;
 
-    public CustomList (){
+    public CustomList() {
         this.customList = new ArrayList<>();
     }
-    
+
+    //•	Add {element} - Adds the given element to the end of the list.
+//•	Remove {index} - Removes the element at the given index.
+//•	Contains {element} - Prints if the list contains the given element (true or false).
+//•	Swap {index1} {index2} - Swaps the elements at the given indexes.
+//•	Greater {element} - Counts the elements that are greater than the given element and prints their count
+//•	Max - Prints the maximum element in the list.
+//•	Min - Prints the minimum element in the list.
+//•	Print - Prints all elements in the list, each on a separate line.
+//•	END - stops the reading of commands.
+    public void add(T element) {
+        customList.add(element);
+    }
+
+    public void remove(int index) {
+        customList.remove(index);
+    }
+
+    public boolean contains(T element) {
+      return customList.contains(element);
+    }
+
+    public void swap(int firstIndex, int secondIndex) {
+        T currentElement = customList.get(firstIndex);
+        customList.set(firstIndex, customList.get(secondIndex));
+        customList.set(secondIndex, currentElement);
+    }
+
+    public int greater(T element) {
+        return (int) customList.stream().filter(e -> e.compareTo(element) > 0).count();
+    }
+    public T max (){
+       return customList.stream().max((f,s)-> f.compareTo(s)).get();
+    }
+    public T min (){
+        return customList.stream().min((f,s)-> f.compareTo(s)).get();
+    }
+    public void sort (){
+        customList.sort(Comparable::compareTo);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        customList.forEach(e ->sb.append(String.format("%s%n",e)));
+        return sb.toString().trim();
+    }
 }
--- Generics/src/GenericsExercises/Main.java
+++ Generics/src/GenericsExercises/Main.java
@@ -7,6 +7,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        CustomList<String> customList = new CustomList<>();
+        String input = scanner.nextLine();
+        while (!input.equals("END")){
+            String command = input.split("\\s+")[0];
+            switch (command){
+                case "Add":
+                    customList.add(input.split(" ")[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(input.split(" ")[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(input.split(" ")[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(input.split(" ")[1]),Integer.parseInt(input.split(" ")[2]));
+                    break;
+                case "Greater":
+                    System.out.println(customList.greater(input.split(" ")[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.max());
+                    break;
+                case "Min":
+                    System.out.println(customList.min());
+                    break;
+                case "Print":
+                    System.out.println(customList.toString());
+                    break;
+                case "Sort":
+                    customList.sort();
+                    break;
+            }
+            input = scanner.nextLine();
+        }
     }
 }
