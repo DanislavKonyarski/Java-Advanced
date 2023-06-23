97889356cd689e2715eed3df1717caa656b434f6
Danislav Konyarski <unstopable46@gmail.com>
2023-06-23 13:34:47 +0300

    new task

---
+++ L2Encapsulation/src/T2AnimalFarm/Chicken.java
@@ -0,0 +1,48 @@
+package T2AnimalFarm;
+
+public class Chicken {
+//    name: String
+//age: int
+    private String name;
+    private int age;
+
+    public Chicken(String name, int age) {
+       setName(name);
+       setAge(age);
+    }
+
+    private void setName(String name) {
+        if (name== null || name.trim().isEmpty()){
+            throw new IllegalArgumentException("Name cannot be empty.");
+        }
+        this.name = name;
+    }
+
+    private void setAge(int age) {
+        if (age<0||age>15){
+            throw new IllegalArgumentException("Age should be between 0 and 15.");
+        }
+        this.age = age;
+    }
+//    +	productPerDay (): double
+//+	toString(): Override
+//-	calculateProductPerDay() : double
+    public double productPerDay (){
+      return calculateProductPerDay();
+    }
+
+    private double calculateProductPerDay(){
+        if (age<6){
+            return 2.0;
+        } else if (age<=11) {
+            return 1.0;
+        } else {
+            return 0.75;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Chicken Chichi (age %d) can produce %.2f eggs per day.",age,calculateProductPerDay());
+    }
+}
