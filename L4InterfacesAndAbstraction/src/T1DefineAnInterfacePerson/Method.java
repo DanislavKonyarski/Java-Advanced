0e1bd76e63e003d9539f386cd9eb27544af75aff
Danislav Konyarski <unstopable46@gmail.com>
2023-07-06 12:06:17 +0300

    new task

---
+++ L4InterfacesAndAbstraction/src/T1DefineAnInterfacePerson/Method.java
@@ -0,0 +1,7 @@
+package T1DefineAnInterfacePerson;
+
+public class Method extends Citizen{
+    public Method(String name, int age) {
+        super(name, age);
+    }
+}
---
+++ L4InterfacesAndAbstraction/src/T1DefineAnInterfacePerson/Person.java
@@ -0,0 +1,7 @@
+package T1DefineAnInterfacePerson;
+
+public interface Person {
+
+    String getName();
+    int getAge();
+}
